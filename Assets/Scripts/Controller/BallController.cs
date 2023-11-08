031046a4f832299ff92d7cf7537e5834b9792d8c
Harjot <121034602+ChasingTimefall@users.noreply.github.com>
2023-11-08 09:44:26 -0400

    Major Changes
    Removed level 1
    Added new level with terrain,water and skybox
    Camera now moves around localplayer

--- Assets/Scripts/Controller/BallController.cs
+++ Assets/Scripts/Controller/BallController.cs
@@ -10,11 +10,13 @@ public class BallController : MonoBehaviour
     bool bInCollisionWithRamp = false;
     float flMoveSpeedPerFrame = 3.5f;
     Rigidbody pRigidBody;
+    Camera CamObj;
 
     Vector3 vecStartPoint;
     void Start()
     {
         pRigidBody = GetComponent<Rigidbody>();
+        CamObj = Camera.main;
         vecStartPoint = transform.position;
     }
 
@@ -55,6 +57,48 @@ public class BallController : MonoBehaviour
       //  }
     }
 
+    private float _dragObjectSpeed = 0.2f;
+    public float rotationSpeed = 75.0f;
+    public float walkSpeed;
+
+
+    private void Move()
+    {
+        // Getting the direction to move through player input
+        float hMove = Input.GetAxis("Horizontal");
+        float vMove = Input.GetAxis("Vertical");
+        float speed = 5.0f;
+
+        // Get directions relative to camera
+        Vector3 forward = CamObj.transform.forward;
+        Vector3 right = CamObj.transform.right;
+
+        // Project forward and right direction on the horizontal plane (not up and down), then
+        // normalize to get magnitude of 1
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+
+        // Set the direction for the player to move
+        Vector3 dir = right * hMove + forward * vMove;
+
+        // Set the direction's magnitude to 1 so that it does not interfere with the movement speed
+        dir.Normalize();
+
+        // Move the player by the direction multiplied by speed and delta time 
+       // transform.position += dir * speed * Time.deltaTime;
+
+        pRigidBody.MovePosition(dir * speed * Time.deltaTime);
+
+        // Set rotation to direction of movement if moving
+        if (dir != Vector3.zero)
+        {
+           // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(forward), 0.2f);
+        }
+
+    }
+
     void Update()
     {
         if (transform.position.y < 0 )
@@ -69,22 +113,26 @@ public class BallController : MonoBehaviour
 
         }
 
+        Move();
+        /*
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        Vector3 vecMove = new Vector3(horizontalInput, 0.0f, verticalInput);
+        transform.position += vecMove;
 
         if (horizontalInput != 0.0f || verticalInput != 0.0f)
         {
-            Vector3 vecMove = new Vector3(horizontalInput, 0.0f, verticalInput);
+
             vecMove.Normalize();
 
-            pRigidBody.velocity = new Vector3(vecMove.x * flMoveSpeedPerFrame, pRigidBody.velocity.y, pRigidBody.velocity.z);
+            pRigidBody.velocity = new Vector3(vecMove.x * flMoveSpeedPerFrame, pRigidBody.velocity.y, vecMove.y * flMoveSpeedPerFrame);
         }
 
-        pRigidBody.velocity = new Vector3(pRigidBody.velocity.x, pRigidBody.velocity.y, 2f * flMoveSpeedPerFrame);
+        //pRigidBody.velocity = new Vector3(pRigidBody.velocity.x, pRigidBody.velocity.y, 2f * flMoveSpeedPerFrame);
 
         if (bInCollisionWithRamp)
             pRigidBody.velocity = new Vector3(pRigidBody.velocity.x, pRigidBody.velocity.y, 8f * flMoveSpeedPerFrame);
-
+        */
 
     }
 
--- Assets/Scripts/Controller/CamController.cs
+++ Assets/Scripts/Controller/CamController.cs
@@ -18,15 +18,76 @@ public class CamController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 cameraPosition = MoveObject.transform.position + vecInitialDelta;
-        transform.position = cameraPosition;
+        //Vector3 cameraPosition = MoveObject.transform.position + vecInitialDelta;
+        //transform.position = cameraPosition;
     }
 
+    public float rotateSpeed = 5.0f;
+    public float zoomSpeed = 20.0f;
 
+    public float distance = 5.0f;
+    public float xSpeed = 120.0f;
+    public float ySpeed = 120.0f;
+
+    public float yMinLimit = -20f;
+    public float yMaxLimit = 80f;
+
+    public float distanceMin = 1.5f;
+    public float distanceMax = 25f;
+    public float distanceDefault = 5.0f;
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+
+    public void ResetCamera()
+    {
+        x = 180;
+        y = 25;
+    }
+
+    float x = 0.0f;
+    float y = 0.0f;
     void Update()
     {
+        //float rotateHorizontal = Input.GetAxis("Mouse X");
+        // float rotateVertical = Input.GetAxis("Mouse Y");
 
-        //  transform.LookAt(MoveObject.transform);
+        x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+        y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+        y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+        Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+        distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, distanceMin, distanceMax);
+
+
+
+        RaycastHit hit;
+        if (Physics.Linecast(MoveObject.transform.position, transform.position, out hit))
+        {
+            distance -= hit.distance;
+        }
+        Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+        Vector3 position = rotation * negDistance + MoveObject.transform.position;
+
+
+
+        transform.rotation = rotation;
+        transform.position = position;
+        //   transform.LookAt(MoveObject.transform);
+
+
+
+        // transform.RotateAround(MoveObject.transform.position, -Vector3.up, rotateHorizontal * flSensitivity); 
+        //  transform.RotateAround(Vector3.zero, transform.right, rotateVertical * flSensitivity); 
+        //    transform.LookAt(MoveObject.transform);
     }
 
 }
