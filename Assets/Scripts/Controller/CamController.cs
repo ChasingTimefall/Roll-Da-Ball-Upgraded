722f2b2272a6d858bee8713a5c1f8d9d87c7ee89
Harjot Singh <121034602+ChasingTimefall@users.noreply.github.com>
2023-12-07 18:17:47 -0400

    Adjusted Camera Control

--- Assets/Scripts/Controller/CamController.cs
+++ Assets/Scripts/Controller/CamController.cs
@@ -5,93 +5,60 @@ using UnityEngine.XR;
 
 public class CamController : MonoBehaviour
 {
-    GameObject MoveObject;
-    float flSensitivity = 19f;
 
-    Vector3 vecInitialDelta;
+    public float sensitivity = 5f;
+    public float minYAngle = -20f;
+    public float maxYAngle = 80f;
+
+    private float currentX = 0f;
+    private float currentY = 0f;
+    private float upwardAdjustment = 1.0f; 
+
+    private Transform parentTransform;
+    public LayerMask collisionLayers; // Layers to check for collision
+
+    private Vector3 offset = new Vector3(4, 4, 4);
+
     void Start()
     {
-        MoveObject = GameObject.Find("DaBall");
-        vecInitialDelta = transform.position - MoveObject.transform.position;
+        parentTransform = transform.parent;
+        transform.localPosition = offset; // Set initial position as the offset
     }
 
-
-    void FixedUpdate()
-    {
-        //Vector3 cameraPosition = MoveObject.transform.position + vecInitialDelta;
-        //transform.position = cameraPosition;
-    }
-
-    public float rotateSpeed = 5.0f;
-    public float zoomSpeed = 20.0f;
-
-    public float distance = 5.0f;
-    public float xSpeed = 120.0f;
-    public float ySpeed = 120.0f;
-
-    public float yMinLimit = -20f;
-    public float yMaxLimit = 80f;
-
-    public float distanceMin = 1.5f;
-    public float distanceMax = 25f;
-    public float distanceDefault = 5.0f;
-
-    public static float ClampAngle(float angle, float min, float max)
-    {
-        if (angle < -360F)
-            angle += 360F;
-        if (angle > 360F)
-            angle -= 360F;
-        return Mathf.Clamp(angle, min, max);
-    }
-
-    public void ResetCamera()
-    {
-        x = 180;
-        y = 25;
-    }
-
-    float x = 0.0f;
-    float y = 0.0f;
     void Update()
     {
-        //float rotateHorizontal = Input.GetAxis("Mouse X");
-        // float rotateVertical = Input.GetAxis("Mouse Y");
-
-        x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
-        y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
-
-        y = ClampAngle(y, yMinLimit, yMaxLimit);
-
-        Quaternion rotation = Quaternion.Euler(y, x, 0);
-
-       // distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, distanceMin, distanceMax);
-
-
-
-       // RaycastHit hit;
-       // if (Physics.Linecast(MoveObject.transform.position, transform.position, out hit))
-       // {
-       //     distance -= hit.distance;
-      //  }
-        Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
-        Vector3 position = rotation * negDistance + MoveObject.transform.position;
-
-        transform.rotation = rotation;
-        transform.position = position;
-
-        //   transform.LookAt(MoveObject.transform);
-
-
-
-        // transform.RotateAround(MoveObject.transform.position, -Vector3.up, rotateHorizontal * flSensitivity); 
-        //  transform.RotateAround(Vector3.zero, transform.right, rotateVertical * flSensitivity); 
-        //    transform.LookAt(MoveObject.transform);
+        currentX += Input.GetAxis("Mouse X") * sensitivity;
+        currentY += Input.GetAxis("Mouse Y") * sensitivity;
+        currentY = Mathf.Clamp(currentY, minYAngle, maxYAngle);
     }
 
+    void LateUpdate()
+    {
+        parentTransform.localRotation = Quaternion.Euler(currentY, currentX, 0);
+       
+        Vector3 desiredCameraPos = parentTransform.TransformPoint(offset);
+        RaycastHit hit;
 
-    public float distanceFromTarget = 5.0f; // Default distance from the target
-    public Vector3 cameraOffset; // Offset of the camera position
+        if (Physics.Linecast(parentTransform.position, desiredCameraPos, out hit))
+        {
 
+            if (!hit.collider.CompareTag("Terrain")) 
+            {
+                Vector3 collisionPoint = hit.point;
+                collisionPoint += transform.up * upwardAdjustment; // Adjusting the camera position upwards
+                transform.position = collisionPoint;
+            }
+            else
+            {
+                transform.position = desiredCameraPos;
+            }
+        }
+        else
+        {
+            transform.position = desiredCameraPos;
+        }
 
+        transform.LookAt(parentTransform.position);
+    }
 }
+
